398e8bfb5d1a1e2ac20524dc3aa4312202a502ab
kibarke <kibarkework@gmail.com>
2024-01-31 14:37:02 -0500

    still working on API key, need to figure out how to use it

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
 const APIKey = c180e5ec2d17abbcfebe00a1ac5f1f74;
 const city = 'Philadelphia';
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c180e5ec2d17abbcfebe00a1ac5f1f74'
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c180e5ec2d17abbcfebe00a1ac5f1f74';
+
+const apiPhilly = "http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=c180e5ec2d17abbcfebe00a1ac5f1f74";
+
+fetch(apiPhilly);
