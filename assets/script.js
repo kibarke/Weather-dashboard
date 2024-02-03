171784c8ab0013bc8bbd37cbe15b4f91bd63e2c0
kibarke <kibarkework@gmail.com>
2024-02-03 12:45:03 -0500

    make it so that if a location doesn't exist, it shows an error

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,9 @@ const container = document.querySelector('.container');
 const search = document.querySelector('.search-box');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+const cityHide = document.querySelector('.city-hide');
+
 
 search addEventListener('click', () => {
     const APIKey = 'c180e5ec2d17abbcfebe00a1ac5f1f74';
@@ -12,11 +15,35 @@ search addEventListener('click', () => {
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metricappid=${APIKey}`).then(response => response.json()).then(json =>
     {
+
+        // if the city is non-existent
+
+        if (json.cod == '404') {
+            cityHide.textContent = city;
+            container.computedStyleMap.height = '400px'
+            weatherBox.classList.remove('active');
+            weatherDetails.classList.remove('active');
+            error404.classList.add('active');
+            return;
+        }
+
         const image = document.querySelector('.weather-box img');
         const tempature = document.querySelector('.weather-box .tempature');
         const description = document.querySelector('.weather-box .description');
         const humidity = document.querySelector('.weather-details .humidity span');
         const wind = document.querySelector('.weather-details .wind span');
+
+        if (cityHide.textContent == city) {
+            return;
+        }
+        else {
+            cityHide.textContent = city;
+
+            container.computedStyleMap.height = '555px'
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error404.classList.remove('active');
+       
 
         // Making it so that when the user looks up the city, they have an image that coordinates with the weather of the city
 
@@ -53,8 +80,12 @@ search addEventListener('click', () => {
                 image.src = 'image/cloud.jpg';
         }
 
+        tempature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
+        description.innerHTML = `${json.weather[0].decription}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        tempature.innerHTML = `${parseInt(json.main.speed)}Mi/h`;
         
-
+        }
     });
 
 });
