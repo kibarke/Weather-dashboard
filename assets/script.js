e086737576995701dffea90ff1fe46d5291fb0d7
kibarke <kibarkework@gmail.com>
2024-02-03 10:56:01 -0500

    working on api and making the search bar locate any city

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,32 @@
-const APIKey = c180e5ec2d17abbcfebe00a1ac5f1f74;
-const city = 'Philadelphia';
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c180e5ec2d17abbcfebe00a1ac5f1f74';
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
 
-const apiPhilly = "http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=c180e5ec2d17abbcfebe00a1ac5f1f74";
+search addEventListener('click', () => {
+    const APIKey = 'c180e5ec2d17abbcfebe00a1ac5f1f74';
+    const city = document.querySelector('.search-box input').value;
 
-fetch(apiPhilly);
+    if (city == '')
+    return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metricappid=${APIKey}`).then(response => response.json()).then(json =>
+    {
+        const image = document.querySelector('.weather-box img');
+        const tempature = document.querySelector('.weather-box .tempature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = '';
+
+            break;
+
+            default:
+                break;
+        }
+    });
+
+});
